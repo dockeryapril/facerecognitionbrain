7214607caf2cfab1841e1212d0e193a787a81d81
aprildockery <dockeryapril@gmail.com>
2022-07-18 14:14:21 -0400

    tilt function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Logo />
+      
+      
 
-
-
-      {/*<Logo />
-      <ImageLinkForm />
+      {/*<ImageLinkForm />
       <FaceRecognition />*/}
     </div>
   );
